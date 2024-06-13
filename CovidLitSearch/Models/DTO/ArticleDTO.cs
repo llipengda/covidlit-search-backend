b88a9a241a04e7b2ee40b3d24f0ceaa49123efeb
llipengda <lipengda2@outlook.com>
2024-06-14 01:05:21 +0800

    update

--- CovidLitSearch/Models/DTO/ArticleDTO.cs
+++ CovidLitSearch/Models/DTO/ArticleDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
 namespace CovidLitSearch.Models.DTO;
@@ -6,14 +7,20 @@ namespace CovidLitSearch.Models.DTO;
 public class ArticleDTO : Article
 {
     [Column("journal_name")]
-    public string JournalName { get; set; } = null!;
+    public string? JournalName { get; set; } = null!;
 
     [JsonIgnore]
     [Column("author")]
-    public string Author { get; set; } = null!;
+    public string? Author { get; set; } = null!;
 
     [NotMapped]
     public IList<string> Authors { get; set; } = [];
+
+    [Column("volume")]
+    public string? Volume { get; set; }
+
+    [Column("pages")]
+    public string? Pages { get; set; }
 
     [NotMapped]
     [JsonIgnore]
--- CovidLitSearch/Services/ArticleService.cs
+++ CovidLitSearch/Services/ArticleService.cs
@@ -1,4 +1,5 @@
-﻿using CovidLitSearch.Models;
+﻿using System.Diagnostics;
+using CovidLitSearch.Models;
 using CovidLitSearch.Models.DTO;
 using CovidLitSearch.Services.Interface;
 using Microsoft.EntityFrameworkCore;
@@ -10,14 +11,50 @@ public class ArticleService(DbprojectContext context) : IArticleService
     public async Task<List<ArticleDTO>> GetArticles(int page, int pageSize)
     {
         page = page <= 0 ? 1 : page;
+        var sw = new Stopwatch();
+        sw.Start();
         var data = await context
             .Database.SqlQuery<ArticleDTO>(
                 $"""
-                SELECT article.*, publish.journal_name, write.author_name AS author
+                SELECT article.*, publish.*, write.author_name AS author
+                FROM article
+                LEFT JOIN publish on article.id = publish.article_id
+                    AND article.url IS NOT NULL
+                LEFT JOIN write on article.id = write.article_id
+                LIMIT {pageSize} OFFSET {(page - 1) * pageSize}
+                """
+            )
+            .AsNoTracking()
+            .ToListAsync();
+        var time1 = sw.ElapsedMilliseconds;
+        await Console.Out.WriteLineAsync($"{time1}");
+        var res = data.GroupBy(a => a.Id)
+            .Select(g =>
+            {
+                var article = g.First();
+                article.Authors = g.Select(a => a.Author!).ToList();
+                if (article.Authors.All(a => a is null))
+                {
+                    article.Authors = [];
+                }
+                return article;
+            })
+            .ToList();
+        var time2 = sw.ElapsedMilliseconds;
+        await Console.Out.WriteLineAsync($"{time2 - time1}");
+        return res;
+    }
+
+    public async Task<Article?> GetArticleById(string id)
+    {
+        var data = await context
+            .Database.SqlQuery<ArticleDTO>(
+                $"""
+                SELECT article.*, publish.*, write.author_name AS author
                 FROM article
                 LEFT JOIN publish on article.id = publish.article_id
                 LEFT JOIN write on article.id = write.article_id
-                LIMIT {pageSize} OFFSET {(page - 1) * pageSize}
+                WHERE id = {id}
                 """
             )
             .AsNoTracking()
@@ -26,18 +63,13 @@ public class ArticleService(DbprojectContext context) : IArticleService
             .Select(g =>
             {
                 var article = g.First();
-                article.Authors = g.Select(a => a.Author).ToList();
-                article.Author = string.Join("; ", article.Authors);
+                article.Authors = g.Select(a => a.Author!).ToList();
+                if (article.Authors.All(a => a is null))
+                {
+                    article.Authors = [];
+                }
                 return article;
             })
-            .ToList();
-    }
-
-    public async Task<Article?> GetArticleById(string id)
-    {
-        return await context
-            .Set<Article>()
-            .FromSqlInterpolated($"SELECT * FROM article WHERE id = {id}")
-            .SingleOrDefaultAsync();
+            .FirstOrDefault();
     }
 }
