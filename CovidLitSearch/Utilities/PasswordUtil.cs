9d940082f79bc5c1322faa249931c8d0aea992b2
llipengda <lipengda2@outlook.com>
2024-06-14 20:46:46 +0800

    password

---
+++ CovidLitSearch/Utilities/PasswordUtil.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace CovidLitSearch.Utilities;
+
+public class PasswordUtil
+{
+    public static string GenerateSalt()
+    {
+        byte[] saltBytes = new byte[16];
+        RandomNumberGenerator.Fill(saltBytes);
+        return Convert.ToBase64String(saltBytes);
+    }
+
+    public static string HashPassword(string password, string salt)
+    {
+        byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+        byte[] saltBytes = Convert.FromBase64String(salt);
+
+        byte[] combinedBytes = new byte[passwordBytes.Length + saltBytes.Length];
+        Buffer.BlockCopy(passwordBytes, 0, combinedBytes, 0, passwordBytes.Length);
+        Buffer.BlockCopy(saltBytes, 0, combinedBytes, passwordBytes.Length, saltBytes.Length);
+        byte[] hashBytes = SHA256.HashData(combinedBytes);
+        return Convert.ToBase64String(hashBytes);
+    }
+
+    public static bool VerifyPassword(string password, string salt, string hashedPassword)
+    {
+        string hashedInput = HashPassword(password, salt);
+        return hashedInput == hashedPassword;
+    }
+}
