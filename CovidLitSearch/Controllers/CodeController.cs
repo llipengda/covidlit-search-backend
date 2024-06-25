7f71d2a6443426c6a8cde80a1d9a96586d3b45fa
XiaoWu <1610365132@qq.com>
2024-06-25 19:09:12 +0800

    2

--- CovidLitSearch/Controllers/CodeController.cs
+++ CovidLitSearch/Controllers/CodeController.cs
@@ -8,6 +8,12 @@ namespace CovidLitSearch.Controllers;
 [Route("api/codes")]
 public class CodeController(ICodeService service) : ControllerBase
 {
+    /// <summary>
+    ///  Send a verification code to the email
+    /// </summary>
+    /// <param name="email"></param>
+    /// <response code="400">Bad Request</response>
+    /// <returns></returns>
     [HttpPost]
     public ActionResult SendCode([Required] string email)
     {
