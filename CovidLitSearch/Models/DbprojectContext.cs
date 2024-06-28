58f21b741ad3d339ebbd761f639f63a272a96143
llipengda <lipengda2@outlook.com>
2024-06-28 22:43:14 +0800

    db context

--- CovidLitSearch/Models/DbprojectContext.cs
+++ CovidLitSearch/Models/DbprojectContext.cs
@@ -2,12 +2,4 @@
 
 namespace CovidLitSearch.Models;
 
-public class DbprojectContext(
-    DbContextOptions<DbprojectContext> options
-) : DbContext(options)
-{
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-        optionsBuilder
-            .UseNpgsql("Name = ConnectionStrings:DBProject")
-            .EnableSensitiveDataLogging();
-}
+public class DbprojectContext(DbContextOptions<DbprojectContext> options) : DbContext(options);
